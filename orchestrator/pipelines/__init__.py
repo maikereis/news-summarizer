c8359a4d125e7a634b2a640f8d4d0779f479ff4b
Maike Reis <maikerdralcantara@gmail.com>
2025-01-04 17:14:09 -0300

    Add RAG data indexing pipeline

--- orchestrator/pipelines/__init__.py
+++ orchestrator/pipelines/__init__.py
@@ -1,6 +1,7 @@
 from .article_cleaner import drop_duplicates
 from .article_scrap import scrap
+from .data_indexing import index_data
 from .links_cleaner import remove_garbage
 from .links_crawl import crawl
 
-__all__ = ["crawl", "remove_garbage", "scrap", "drop_duplicates"]
+__all__ = ["crawl", "remove_garbage", "scrap", "drop_duplicates", "index_data"]
