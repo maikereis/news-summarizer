2503232fd64bac1c54a0ec2b4d42519cd61d4e9d
maike reis <maikerdralcantara@gmail.com>
2025-06-08 23:38:38 -0300

    update: pipelines

--- orchestrator/pipelines/generate_dataset.py
+++ orchestrator/pipelines/generate_dataset.py
@@ -1,9 +1,18 @@
-from steps.generate_datasets import get_prompts, load
+from steps.datasets import generate_datasets, load, to_hugging
 from zenml import pipeline
 
 
 @pipeline(enable_cache=False)
-def generate():
-    cleaned_articles = load()
-    last_step = get_prompts(cleaned_articles)
-    return last_step.invocation_id
+def generate(dataset_type: str = "preference") -> None:
+    if dataset_type == "preference":
+        cleaned_articles = load(2)
+        dataset = generate_datasets(cleaned_articles, dataset_type)
+        to_hugging("maikerdr/brazilian-news-article-summarization-DPO", dataset)
+
+    elif dataset_type == "summarization":
+        cleaned_articles = load(2)
+        dataset = generate_datasets(cleaned_articles, dataset_type)
+        to_hugging("maikerdr/brazilian-news-article-summarization", dataset)
+
+    else:
+        raise ValueError(f"Unsupported dataset_type: {dataset_type}. Must be 'preference' or 'summarization'.")
--- orchestrator/steps/data_indexing/__init__.py
+++
@@ -1,6 +0,0 @@
-from .chunk_and_embed import vectorize
-from .clean_documents import clean
-from .load_documents import load
-from .store_vectors import store
-
-__all__ = ["load", "clean", "vectorize", "store"]
